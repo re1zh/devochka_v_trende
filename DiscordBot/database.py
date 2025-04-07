4f216a8cfa76341f863c92d9afc41c0414cc6995
Едисеев Олег <ediseev.oleg@wb.ru>
2025-04-07 22:13:57 +0300

    docker

--- DiscordBot/database.py
+++ DiscordBot/database.py
@@ -1,20 +1,13 @@
 from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import json
+import os
 
-try:
-    with open('db_credits.json') as config_file:
-        config = json.load(config_file)
-
-    POSTGRES_USER = config.get('POSTGRES_USER')
-    POSTGRES_PASS = config.get('POSTGRES_PASS')
-    HOST = config.get('HOST')
-    PORT = config.get('PORT')
-    DATABASE = config.get('DATABASE')
-
-except Exception as e:
-    raise ValueError(f"Ошибка при загрузке конфигурации: {str(e)}")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASS = os.getenv("POSTGRES_PASS")
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+DATABASE = os.getenv("DATABASE")
 
 DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASS}@{HOST}:{PORT}/{DATABASE}"
 
@@ -24,7 +17,6 @@ Base = declarative_base()
 
 class Song(Base):
     __tablename__ = "songs"
-
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, nullable=False)
     path = Column(String, nullable=False)
